590f6f52ddc6149a92ec57709e337ce8925a0cff
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2022-07-10 16:03:35 +0900

    Create git-config.sh and divide

---
+++ git-conf.sh
@@ -0,0 +1,8 @@
+echo '\n\nInitializing Git'
+echo ' 1) Main branch setup master -> main'
+git config --global init.defaultBranch main
+echo ' 2) Add your information'
+read -p "  • User name: " userName
+read -p "  • User email: " userEmail
+git config --global user.name $userName
+git config --global user.email $userEmail
