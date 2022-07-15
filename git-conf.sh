e82a7ee14db784f031df43985da3e2b29471a9cc
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2022-07-15 21:08:20 +0900

    Fix $userName error

--- git-conf.sh
+++ git-conf.sh
@@ -4,5 +4,7 @@ git config --global init.defaultBranch main
 echo ' 2) Add your information'
 read -p "  • User name: " userName
 read -p "  • User email: " userEmail
-git config --global user.name $userName
-git config --global user.email $userEmail
+git config --unset --global user.name
+git config --unset --global user.email
+git config --global user.name "$userName"
+git config --global user.email "$userEmail"
