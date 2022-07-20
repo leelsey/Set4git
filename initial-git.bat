4ff3a6e61c15a71abd6624c679631a7f9f0a208c
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2022-07-20 21:35:23 +0900

    Create initial-git for Windows

---
+++ initial-git.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+echo Installing Git with Git LFS
+echo.
+choco install -y git git-lfs gh
+echo ----> Done!
+call "git-conf.bat"
+echo ----> Done!
+echo.
+echo.
+echo Check your git config
+git config --list
