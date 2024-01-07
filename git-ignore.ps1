425f0d74959d5b6ee0c6c27c660a6699cf7b2060
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2024-01-07 21:53:20 +0900

    Fix git-ignore.ps1 path error

--- git-ignore.ps1
+++ git-ignore.ps1
@@ -1,11 +1,16 @@
-Write-Host "Setup git ignore"
+Write-Host "`nSetup git ignore"
 
-if (Test-Path "$env:userprofile\gitignore_global") {
-    Remove-Item "$env:userprofile\gitignore_global"
+if (!(Test-Path "$env:userprofile\.config\git")) {
+    New-Item -ItemType Directory -Force -Path "$env:userprofile\.config\git"
 }
-git config --global core.excludesfile "$env:userprofile\.gitignore_global"
-Write-Host "  • Generated git ingnore file: $env:userprofile\.gitignore_global"
+if (Test-Path "$env:userprofile\.config\git\.gitignore_global") {
+    Remove-Item "$env:userprofile\.config\git\.gitignore_global"
+}
 
-Copy-Item .\gitignore-sample\windows "$env:userprofile\.gitignore_global"
+git config --global --unset core.excludesfile
+git config --global core.excludesfile "$env:userprofile\.config\git\.gitignore_global"
+Write-Host "  • Generated git ingnore file: $env:userprofile\.config\git\.gitignore_global"
+
+Copy-Item .\gitignore-sample\windows "$env:userprofile\.config\git\.gitignore_global"
 Write-Host "  • Updated your global git ignore"
 Write-Host "Done!"
