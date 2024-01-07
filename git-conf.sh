4ff4ec09bc7859e8b10ef0ade1293f7c0fd6c5c5
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2024-01-07 19:17:14 +0900

    Update git-config.sh

--- git-conf.sh
+++ git-conf.sh
@@ -1,15 +1,24 @@
-echo '\n\nInitializing Git'
-echo ' 1) Default configure'
-git config --global init.defaultBranch main
-echo '  • Main branch default name changed master -> main'
-git config --global color.ui true
-echo '  • Colourising enabled'
-git config --global core.editor vi
-echo '  • Editor set to vi (vim)'
-echo ' 2) Add your information'
+#! /usr/bin/env bash
+
+echo ''
+echo 'Configuring Git'
+
+echo ' 1) Git configuration for user and email'
 read -p "  • User name: " userName
 read -p "  • User email: " userEmail
 git config --unset --global user.name
 git config --unset --global user.email
 git config --global user.name "$userName"
 git config --global user.email "$userEmail"
+
+echo ' 2) Git configuration for default'
+git config --global init.defaultBranch main
+echo '  • Git main branch default "main"'
+git config --global color.ui true
+echo '  • Git Colourising enabled'
+git config --global core.editor vi
+echo '  • Git editor set to vi (vim)'
+git lfs install
+echo '  • Git LFS installed'
+
+echo 'Done!'
