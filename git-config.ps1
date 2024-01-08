f8919d627e485262a3030fabf60bf40652686446
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2024-01-08 13:55:53 +0900

    Add config all colour

--- git-config.ps1
+++ git-config.ps1
@@ -10,10 +10,14 @@ git config --global user.email "$gitUserEmail"
 Write-Host " 2) Git configuration for default"
 git config --global init.defaultBranch main
 Write-Host "  • Git main branch default 'main'"
-git config --global color.ui true
+git config --global color.ui auto
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
 Write-Host "  • Git Colourising enabled"
-git config --global core.editor vi
-Write-Host "  • Git editor set to vi (vim)"
+git config --global core.editor notepad
+Write-Host "  • Git editor set to Notepad"
 git lfs install
 Write-Host "  • Git LFS installed"
 
--- git-config.sh
+++ git-config.sh
@@ -14,10 +14,14 @@ git config --global user.email "$userEmail"
 echo ' 2) Git configuration for default'
 git config --global init.defaultBranch main
 echo '  • Git main branch default "main"'
-git config --global color.ui true
+git config --global color.ui auto
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
 echo '  • Git Colourising enabled'
 git config --global core.editor vi
-echo '  • Git editor set to vi (vim)'
+echo '  • Git editor set to vi(Vim)'
 git lfs install
 echo '  • Git LFS installed'
 
